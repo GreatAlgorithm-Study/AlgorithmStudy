64724191f3e4526cef9fb6f928be5ab9ca1229bd
yeahdy <yejine98@naver.com>
2024-09-12 18:33:11 +0900

    이예진: [PG] 12927 야근지수_240912

---
+++ Programmers/Level3/YJ_야근지수.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class YJ_야근지수 {
+    public static void main(String[] args) {
+        int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
+        System.out.println(solution(topping));
+    }
+
+    static int solution(int[] topping) {
+
+    }
+}
