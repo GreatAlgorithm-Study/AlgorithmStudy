10762167789619e76eaec8afa180220537a595eb
KodaHye <kodhdh09@gmail.com>
2024-09-13 10:29:59 +0900

    고다혜: [PG] 154539 뒤에 있는 큰 수 찾기_240913

---
+++ Programmers/Level2/DH_154539.java
@@ -0,0 +1,30 @@
+package Programmers.Level2;
+import java.util.*;
+
+/*
+뒤에 있는 큰 수 찾기
+ */
+
+class DH_154539 {
+    public int[] solution(int[] numbers) {
+
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer, - 1);
+
+        Stack<Integer> stack = new Stack();
+        for(int i = numbers.length - 1; i >= 0; i--) {
+
+            if(!stack.isEmpty() && numbers[i] >= stack.peek()) {
+                while(!stack.isEmpty() && numbers[i] >= stack.peek()) {
+                    stack.pop();
+                }
+            }
+
+            if(!stack.isEmpty()) answer[i] = stack.peek();
+            stack.push(numbers[i]);
+        }
+
+        return answer;
+    }
+}
+
