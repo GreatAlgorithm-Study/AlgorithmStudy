ae53f0d222c6eaa68e8afa586613c4eacc1ca505
KodaHye <kodhdh09@gmail.com>
2024-09-13 10:29:12 +0900

    고다혜: [PG] 132265 롤케이크 자르기_240912

---
+++ Programmers/Level2/DH_132265.java
@@ -0,0 +1,34 @@
+package Programmers.Level2;
+
+/*
+롤케이크_자르기
+ */
+
+class DH_132265 {
+
+    public int solution(int[] topping) {
+        int length = topping.length;
+        int toppingType = 0, answer = 0;
+
+        int cnt[] = new int[10_001];
+        for(int i = 0; i < length; i++) {
+            if(cnt[topping[i]] == 0) toppingType++;
+            cnt[topping[i]]++;
+        }
+
+        int cntA = 0;
+        boolean check[] = new boolean[10_001];
+
+        for(int i = 0; i < length; i++) {
+            if(!check[topping[i]]) {
+                cntA++;
+               check[topping[i]] = true;
+            }
+
+            cnt[topping[i]]--;
+            if(cnt[topping[i]] == 0) toppingType--;
+            if(cntA == toppingType) answer++;
+        }
+        return answer;
+    }
+}
