e9618f2717638c7228e5519ffb526bda727168ca
yeahdy <yejine98@naver.com>
2024-09-16 10:13:03 +0900

    이예진: [PG] 49994 방문길이_240913 (BFS 구현)

--- Programmers/Level2/YJ_방문길이.java
+++ Programmers/Level2/YJ_방문길이.java
@@ -1,63 +1,70 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * 알고리즘: 단순 구현?
+ * 알고리즘: BFS
  * 시간복잡도: O(n)
  * 아이디어:
- *
+ * BFS로 순회할때 방문한 지점을 방문처리 하는 것이 아닌 다녀간 길을 방문처리 해야 함
+ * 다녀간 길을 표시하는 방법?
+ * 
  */
 public class YJ_방문길이 {
     public static void main(String[] args) {
-//        String dirs = "ULURRDLLU";  //7
-        String dirs = "LULLLLLLU";  //7
+        String dirs = "ULURRDLLU";  //7
+//        String dirs = "LULLLLLLU";  //7
         System.out.println(solution(dirs));
     }
 
+    static int answer = 0;
+    static int[][] direction = {{0,1},{0,-1},{-1,0},{1,0}}; //상,하,좌,우
+    static boolean[][] visited = new boolean[11][11];
+    static Queue<int[]> queue = new LinkedList<>();
     static int solution(String dirs) {
-        int answer = 0;
-        boolean[][] visited = new boolean[11][11];
-        String[] direction = dirs.split("");
+        String[] input = dirs.split("");
+        int[] start = {5,5};
+        int[] end = {11,11};
 
-        //현재위치 저장
-        int x =5;
-        int y =5;
-        visited[x][y] = true;
-        boolean isvisited = false;
-        //U:(0,1), D:(0,-1), R:(1,0), L:(-1,0)
-        for(String d : direction){
-            switch(d){
+        //현재위치 방문처리
+        queue.offer(start);
+        visited[start[0]][start[1]] = true;
+
+        for(String i : input){
+            switch(i){
                 case "U":
-                    isvisited = visited[x][y+1];
-                    if(!isvisited && 11 > y+1){
-                        visited[x][y+1] = true;
-                        y = y+1;
-                        answer++;
-                    }
-                case "D":
-                    isvisited = visited[x][y-1];
-                    if(!isvisited && 0 < y-1){
-                        visited[x][y-1] = true;
-                        y = y-1;
-                        answer++;
-                    }
+                    bfs(visited,direction[0]);
                     break;
-                case "R":
-                    isvisited = visited[x+1][y];
-                    if(!isvisited){
-                        visited[x+1][y] = true;
-                        x = x+1;
-                        answer++;
-                    }
+                case "D":
+                    bfs(visited,direction[1]);
                     break;
                 case "L":
-                    isvisited = visited[x-1][y];
-                    if(!isvisited && 0 < x-1){
-                        visited[x-1][y] = true;
-                        x = x-1;
-                        answer++;
-                    }
+                    bfs(visited,direction[2]);
+                    break;
+                case "R":
+                    bfs(visited,direction[3]);
                     break;
             }
         }
+        return answer;
+    }
 
-        return answer;  //캐릭터가 처음 걸어본 길의 길이
+    private static void bfs(boolean[][] visited, int[] direction){
+        int[] current = queue.peek();
+        //위치 이동
+        int nx = current[0] + direction[0];
+        int ny = current[1] + direction[1];
+
+        if(nx < 0 || nx > visited[0].length -1 || ny < 0 || ny > visited.length-1){
+            return;
+        }
+        if(visited[nx][ny]){
+            return;
+        }
+        //방문처리
+        queue.poll();
+        visited[nx][ny] = true;
+        //이동값 갱신
+        queue.offer(new int[]{nx,ny});
+        answer++;
     }
 }
