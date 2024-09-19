7fbe140df231f682579278fec5b7f873ca9af9c0
Jewan1120 <hawaii1468@gmail.com>
2024-09-19 09:37:54 +0900

    백제완: [SQL] 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기_240919

---
+++ SQL/2주차/JW_조회수가_가장_많은_중고거래_게시판의_첨부파일_조회하기.sql
@@ -0,0 +1,23 @@
+WITH CTE AS (
+    SELECT
+        BOARD_ID
+    FROM
+        USED_GOODS_BOARD
+    ORDER BY
+        VIEWS DESC
+    LIMIT 1
+)
+ 
+SELECT
+    CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM 
+    USED_GOODS_FILE
+WHERE
+    BOARD_ID = (
+        SELECT
+            BOARD_ID
+        FROM
+            CTE
+    )
+ORDER BY
+    FILE_ID DESC
