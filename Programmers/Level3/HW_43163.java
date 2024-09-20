08894ff7438954d59be9c85aae89d2767bf453bc
icegosimperson <hooho931@gmail.com>
2024-09-20 10:56:35 +0900

    이혜원: [PG] 43163 단어 변환_240920

---
+++ Programmers/Level3/HW_43163.java
@@ -0,0 +1,41 @@
+// 최소 단어 변환 과정
+// 단계별로 진행하여 최단 경로 찾기 -> BFS
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        Queue<String> queue = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+        queue.add(begin);
+        int answer = 0;
+
+        while(!queue.isEmpty()){
+            answer++;
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                String current = queue.poll();
+                for(int j=0; j<words.length; j++){
+                    if(!visited[j] && convert(current, words[j])){
+                        if(words[j].equals(target)){
+                            return answer;
+                        }
+                        visited[j] = true;
+                        queue.add(words[j]);
+                    }
+                }
+            }
+        }
+        return 0; // 변환 할 수 없을 경우 0 출력
+    }
+    public boolean convert(String word1, String word2){ // 최소 변환 과정 : 한글자만 다를 경우 변환
+        int dif = 0;
+        for(int i=0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i)){
+                dif++;
+            }
+            if(dif >1) return false;
+        }
+        return dif == 1;
+    }
+
+}
