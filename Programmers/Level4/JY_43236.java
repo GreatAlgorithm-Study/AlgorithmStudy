dbe61f639307b753bd89c16fd032253c4c787b79
yeong <abcd23589534@gmail.com>
2024-09-26 21:20:15 +0900

    이지영: [PG] 43236 징검다리_240926

---
+++ Programmers/Level4/JY_43236.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        List<Integer> rList = new ArrayList<>();
+        for(int i=0; i<rocks.length; i++) {
+            rList.add(rocks[i]);
+        }
+        rList.add(distance);        // 도착지점 추가
+        Collections.sort(rList);
+        
+        int s = 0;
+        int e = distance;
+        while(s <= e) {
+            int mid = (s + e) / 2;
+            
+            int cnt = 0;    // 제거해야할 바위 수
+            int pre = 0;    // 바로 직전의 바위
+            for(int r: rList) {
+                int dist = r - pre;
+                // 두 바위 사이거리가 최소 거리보다 작음 => 안됨, 제거
+                if(dist < mid) cnt++;
+                else pre = r;
+                
+                if(cnt > n) break;
+            }
+            
+            // 없애야할 바위가 n보다 작거나 같으면 정답 저장
+            if(cnt <= n) {
+                answer = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
+
