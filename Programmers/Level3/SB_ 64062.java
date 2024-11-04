59c4508b870f4d108b871dff378b28c55244ff1e
baexxbin <soo3886@gmail.com>
2024-11-04 14:10:45 +0900

    배수빈: [PG] 64062 징검다리 건너기_241104

---
+++ Programmers/Level3/SB_ 64062.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static boolean canGo(int[] stones, int k, int num) {
+        int seq = 0;
+        for (int s : stones) {
+            if (s-num <=0) seq++;
+            else seq = 0;
+            if (seq >= k) return false;
+        }
+        return true;
+    }
+    public static int solution(int[] stones, int k) {
+        int st = 1;             // 가장 적게 건너는 사람 수
+        int ed = 0;             // 가장 많이 건널 수 있는 경우
+        for (int s : stones) {
+            ed = Math.max(ed, s);
+        }
+
+        while (st < ed) {
+            int mid = (st + ed) / 2;
+            if (canGo(stones, k, mid)) st = mid+1;
+            else ed = mid;
+        }
+
+        return st;
+    }
+}
