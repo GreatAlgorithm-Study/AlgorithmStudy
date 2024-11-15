ffce1db1c472cd72d990a7317091734f36d114af
KodaHye <kodhdh09@gmail.com>
2024-11-15 18:22:33 +0900

    고다혜: [PG] 42884 단속 카메라_241115

---
+++ Programmers/Level3/DH_150366.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+class DH_150366 {
+	static class Point {
+		int r, c;
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static String[][] table;
+	static int[][] markGroupNum;
+	static HashMap<Integer, HashSet<Point>> hashMap;
+	
+	static ArrayList<String> solution(String[] commands) {
+		hashMap = new HashMap<Integer, HashSet<Point>>();
+		ArrayList<String> answer = new ArrayList<>();
+		table = new String[51][51];
+		markGroupNum = new int[51][51];
+		
+		for(int r = 1; r < 51; r++) {
+			for(int c = 1; c < 51; c++) {
+				int key = r * 51 + c;
+				hashMap.put(key, new HashSet<>());
+				hashMap.get(key).add(new Point(r, c));
+				markGroupNum[r][c] = key;
+			}
+		}
+		
+		for(String command: commands) {
+			String[] input = command.split(" ");
+			String order = input[0];
+			
+			if(order.equals("UPDATE")) {
+				if(input.length == 4) {
+					int r = getPoint(input, 1);
+					int c = getPoint(input, 2);
+					String value = input[3];
+					update(r, c, value);
+				}
+				if(input.length == 3) {
+					String value1 = input[1];
+					String value2 = input[2];
+					update(value1, value2);
+				}
+			} 
+//			
+			if(order.equals("MERGE")) {
+				int r1 = getPoint(input, 1);
+				int c1 = getPoint(input, 2);
+				int r2 = getPoint(input, 3);
+				int c2 = getPoint(input, 4);
+				
+				merge(r1, c1, r2, c2);
+			}
+			if(order.equals("UNMERGE")) {
+				int r = getPoint(input, 1);
+				int c = getPoint(input, 2);
+				unmerge(r, c);
+			}
+			if(order.equals("PRINT")) {
+				int r = getPoint(input, 1);
+				int c = getPoint(input, 2);
+				answer.add(print(r, c));
+			}
+		}
+		
+		return answer;
+	}
+	
+	static String print(int r, int c) {
+		return table[r][c] == null ? "EMPTY": table[r][c];
+	}
+	
+	static void unmerge(int r, int c) {
+		int key = markGroupNum[r][c];
+		
+		ArrayList<Point> removeSet = new ArrayList<Point>();
+		for(Point p: hashMap.get(key)) {
+			if(p.r == r && p.c == c) continue;
+			
+			int nextKey = p.r * 51 + p.c;
+			markGroupNum[p.r][p.c] = nextKey;
+			table[p.r][p.c] = null;
+			removeSet.add(p);
+			hashMap.get(nextKey).add(p);
+		}
+		
+		hashMap.get(key).removeAll(removeSet);
+	}
+	
+	static void merge(int r1, int c1, int r2, int c2) {
+		if(r1 == r2 && c1 == c2) return;
+		
+		int key = markGroupNum[r1][c1];
+		String value = table[r1][c1];
+		int removeKey = markGroupNum[r2][c2];
+		
+		if(key == removeKey) return;
+		
+		if(value == null && table[r2][c2] != null) {
+			key = removeKey;
+			removeKey = markGroupNum[r1][c1];
+			value = table[r2][c2];
+		}
+		
+		for(Point p: hashMap.get(removeKey)) {
+			table[p.r][p.c] = value;
+			markGroupNum[p.r][p.c] = key;
+		}
+		
+		hashMap.get(key).addAll(hashMap.get(removeKey));
+		hashMap.get(removeKey).clear();
+	}
+	
+	static void update(String value1, String value2) {
+		for(int r = 1; r < table.length; r++) {
+			for(int c = 1; c < table[0].length; c++) {
+				if(table[r][c] == null || !table[r][c].equals(value1)) continue;
+				
+				int key = markGroupNum[r][c];
+				
+				for(Point p: hashMap.get(key)) table[p.r][p.c] = value2;
+			}
+		}
+	}
+	
+	static void update(int r, int c, String value) {
+		int key = markGroupNum[r][c];
+		for(Point p: hashMap.get(key)) table[p.r][p.c] = value;
+	}
+	
+	static int getPoint(String[] input, int idx) {
+		return Integer.parseInt(input[idx]);
+	}
+	
+	public static void main(String[] args) {
+		String[] commands = {"UPDATE 1 1 apple", "MERGE 1 1 2 2", "MERGE 2 2 3 3", "UNMERGE 1 1", "UNMERGE 2 2", "PRINT 1 1", "PRINT 2 2", "PRINT 3 3"};
+		System.out.println(Arrays.toString(solution(commands).toArray()));
+	}
+}
