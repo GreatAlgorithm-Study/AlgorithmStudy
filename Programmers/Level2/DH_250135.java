97aa32dd7fabc35002e0cdd674a87df37f3eb5e5
KodaHye <kodhdh09@gmail.com>
2024-12-07 01:22:26 +0900

    고다혜: [PG] 250135 아날로그 시계_241205

--- Programmers/Level2/DH_250135.java
+++ Programmers/Level2/DH_250135.java
@@ -9,37 +9,44 @@ public class DH_250135 {
 
     	int answer = 0;
     	
-    	// 정각에 시작하는 경우
-        if(s1 == 0 && m1 == 0) answer += 1;
-        
-        // 시작 시간이 끝나는 시간 이전일 때 까지
-        while(h1 * 3600 + m1 * 60 + s1 < h2 * 3600 + m2 * 60 + s2) {
-        	// 자정, 정오가 되는 경우
-        	if((h1 == 11 || h1 == 23) && m1 == 59 && s1 == 59) answer += 1;
-        	else {
-        		// 분침과 초침이 만난 경우
-        		// 정각 제외
-        		if(m1 == s1 && !(m1 == 0 && s1 == 0)) answer += 1;
-        		
-        		// 시침과 초침이 만난 경우
-        		// 자정, 정오가 아닌 시간
-        		if(((h1 % 12) * 5 + m1 / 12) == s1 && !((h1 % 12 == 0) && m1 == 0 && s1 == 0)) answer += 1;
-        	}
-        	
-        	// 초 업데이트
-        	s1 += 1;
-        	if(s1 == 60) m1 += 1;
-        	if(m1 == 60) h1 += 1;
-
-        	s1 %= 60;
-        	m1 %= 60;
-        }
-                
+    	int startTimeToSec = getSec(h1, m1, s1);
+    	int endTimeToSec = getSec(h2, m2, s2);
+    	
+    	// 이전 시, 분 초침 각도
+    	double phr = 0, pmr = 0, psr = 0;
+    	
+    	for(int i = startTimeToSec; i < endTimeToSec + 1; i++) {
+    		double chr = (i / 120.0) % 360;
+    		double cmr = (i / 10.0) % 360;
+    		double csr = (i % 60.0) * 6;
+    		
+    		// 시침과 초침이 겹치는 경우
+    		if(chr < csr && phr > psr) answer += 1;
+    		else if(chr == csr) answer += 1;
+    		// 정각
+    		else if(phr > psr && csr == 0) answer += 1;
+    		
+    		/// 분침과 겹치는 경우
+    		if(cmr < csr && pmr > psr) answer += 1;
+    		else if(cmr == csr) answer += 1;
+    		else if(pmr > psr && csr == 0) answer += 1;
+    		
+    		// 모두 겹치는 경우
+    		if(chr == 0 && cmr == 0 && csr == 0) answer -= 1;
+    		
+    		phr = chr; 
+    		pmr = cmr; 
+    		psr = csr;
+    	}
         return answer;
     }
     
+    static int getSec(int h, int m, int s) {
+    	return h * 60 * 60 + m * 60 + s;
+    }
+    
     public static void main(String[] args) {
-    	int h1 = 0, m1 = 5, s1 = 30, h2 = 0, m2 = 7, s2 = 0;
+    	int h1 = 0, m1 = 6, s1 = 1, h2 = 0, m2 = 6, s2 = 6;
 		System.out.println(solution(h1, m1, s1, h2, m2, s2));
 	}
 }
