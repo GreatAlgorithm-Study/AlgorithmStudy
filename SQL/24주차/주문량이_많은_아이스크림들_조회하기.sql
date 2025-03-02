a0f47fd6270947380710b872c5397a15c1606bee
Jewan1120 <hawaii1468@gmail.com>
2025-03-02 14:28:17 +0900

    백제완: [SQL] 주문량이 많은 아이스크림들 조회하기_250226

---
+++ SQL/24주차/주문량이_많은_아이스크림들_조회하기.sql
@@ -0,0 +1,21 @@
+WITH TOTAL AS (
+    SELECT
+        *
+    FROM
+        JULY 
+    UNION ALL
+    SELECT
+        *
+    FROM
+        FIRST_HALF
+)
+
+SELECT
+    FLAVOR
+FROM
+    TOTAL
+GROUP BY
+    FLAVOR
+ORDER BY
+    SUM(TOTAL_ORDER) DESC
+LIMIT 3
